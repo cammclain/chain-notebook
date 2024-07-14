d484efcfcc216a2aed517ca14510ba028427f38f
cam <cammclain@skiff.com>
2024-07-14 17:03:47 -0500

    make all configs type defined

--- chains/config/config.py
+++ chains/config/config.py
@@ -7,14 +7,14 @@ load_dotenv()
 class Config:
     
     
-    SQLITE_DB_URL = os.getenv('SQLITE_DB_URL')
+    SQLITE_DB_URL: str = os.getenv('SQLITE_DB_URL')
     
-    OLLAMA_BASE_URL = os.getenv('OLLAMA_BASE_URL')
+    OLLAMA_BASE_URL: str = os.getenv('OLLAMA_BASE_URL')
     
     
     ## API KEYS
-    LANGCHAIN_TRACING_V2 = os.getenv('LANGCHAIN_TRACING_V2')
-    LANGCHAIN_API_KEY = os.getenv('LANGCHAIN_API_KEY')
+    LANGCHAIN_TRACING_V2: str = os.getenv('LANGCHAIN_TRACING_V2')
+    LANGCHAIN_API_KEY: str = os.getenv('LANGCHAIN_API_KEY')
     
     
-    TAVILY_API_KEY = os.getenv('TAVILY_API_KEY')
+    TAVILY_API_KEY: str = os.getenv('TAVILY_API_KEY')
