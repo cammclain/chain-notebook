4ae12c27b4b0ae47e01d1adf6e820f8c89280367
cam <cammclain@skiff.com>
2024-07-14 16:34:56 -0500

    config.py updated to match .env.example

--- chains/cyber_security/config/config.py
+++ chains/cyber_security/config/config.py
@@ -6,4 +6,13 @@ load_dotenv()
 
 class Config:
     
+    
     SQLITE_DB_URL = os.getenv('SQLITE_DB_URL')
+    
+    
+    ## API KEYS
+    LANGCHAIN_TRACING_V2 = os.getenv('LANGCHAIN_TRACING_V2')
+    LANGCHAIN_API_KEY = os.getenv('LANGCHAIN_API_KEY')
+    
+    
+    TAVILY_API_KEY = os.getenv('TAVILY_API_KEY')
